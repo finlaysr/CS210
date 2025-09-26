9d453032a7ae0c2bef8d33ec11817c8423e1104f
finlaysr <60317020+finlaysr@users.noreply.github.com>
2025-09-26 17:00:46 +0100

    Updated print to correct output

--- excercises/excercise03.c
+++ excercises/excercise03.c
@@ -9,5 +9,6 @@ int main(void) {
   double d = 2.2;
   int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
   char str[] = "Hello";
-  printf("%d, %c\n", c, i);
+
+  printf("%c, %d, %d, %ld, %f, %lf, %d, %s\n", c, s, i, l, f, d, arr[4], str);
 }
