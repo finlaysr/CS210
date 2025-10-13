784118622c3423382eba65549cf29ed99c95edb2
finlaysr <60317020+finlaysr@users.noreply.github.com>
2025-10-13 16:14:50 +0100

    Finished simple chess challenge

--- excercises/simplechess.c
+++ excercises/simplechess.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void getInput(char *letter, int *num);
+void getPosition(int *xy, char *message, int checkEmpty);
 void movePiece(char letter, int num);
 void printBoard();
 
@@ -14,46 +15,65 @@ char board[8][8] = {{'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
                     {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}};
 
 int main(void) {
-  char l;
-  int n;
   printBoard();
-  while (l != 'q') {
-    printf("\nSelect piece: ");
-    getInput(&l, &n);
-    printf("\n");
-    printf("Contents: %c, %d\n", l, n);
-    movePiece(l, n);
+  while (1) {
+    int prev_xy[2];
+    int new_xy[2];
+    getPosition(prev_xy, "\nSelect piece: ", 0);
+    getPosition(new_xy, "Choose position: ", 1);
+
+    char piece = board[prev_xy[1]][prev_xy[0]];
+    board[prev_xy[1]][prev_xy[0]] = ' ';
+    board[new_xy[1]][new_xy[0]] = piece;
+
+    printBoard();
   }
 }
 
-void getInput(char *letter, int *num) {
-  char input[3];
-  scanf("%3c", input);
+void getPosition(int *xy, char *message, int checkEmpty) {
+  int valid = 0;
+  char letter;
+  int num;
+  while (!valid) {
+    valid = 1;
+    char input[4];
 
-  if (input[0] != 0) {
-    *letter = input[0];
+    printf("%s", message);
+    fgets(input, 4, stdin);
+
+    if (input[0] == 0 || input[1] == 0) {
+      printf("Please enter a position, eg F4\n");
+      valid = 0;
+    }
+
+    if (input[0] != 0) {
+      letter = input[0];
+    }
+    if (input[1] != 0) {
+      num = input[1] - '0';
+    }
+
+    if (letter == 'q') {
+      exit(0);
+    }
+
+    if (letter < 65 || letter > 72 || num < 1 || num > 8) {
+      printf("Please enter a position between A1 and H8\n");
+      valid = 0;
+    }
+
+    if (!checkEmpty && (board[8 - num][letter - 'A'] == ' ')) {
+      printf("Please choose a piece to move\n");
+      valid = 0;
+    }
+
+    if (checkEmpty && (board[8 - num][letter - 'A'] != ' ')) {
+      printf("Please choose an empty space to move to\n");
+      valid = 0;
+    }
   }
-  if (input[1] != 0) {
-    *num = input[1] - '0';
-  }
-}
-
-void movePiece(char letter, int num) {
-  if (letter < 65 || letter > 72 || num < 1 || num > 8) {
-    printf("Invalid input, must be between A1 and H8!\n");
-    return;
-  }
-  int xy[2] = {letter - 'A', 8 - num};
-  printf("x: %d, y: %d\n", xy[0], xy[1]);
-
-  if (board[xy[1]][xy[0]] != ' ') {
-    printf("Invalid space!\n");
-    return;
-  }
-
-  board[xy[1]][xy[0]] = 'x';
-
-  printBoard();
+  xy[0] = letter - 'A';
+  xy[1] = 8 - num;
 }
 
 void printBoard() {
