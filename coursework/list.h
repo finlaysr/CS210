d26d00cb5ff068c1d1da5b5649915a2e5652b153
finlaysr <60317020+finlaysr@users.noreply.github.com>
2025-11-07 14:10:50 +0000

    ACE3 Part 1 done

--- coursework/list.h
+++ coursework/list.h
@@ -1,8 +1,10 @@
 // Create node struct with a data and next field
 typedef struct NODE {
-  char *value[100];
+  char *value;
   struct NODE *next;
 } Node;
 
 // Create new node with string value
-Node *new_node(char value[100]);
+Node *new_node(char *value);
+
+void delete_node(Node *node);
