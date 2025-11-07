cb04f848fb8604d8c86444a2381bc6916ff89ba8
finlaysr <60317020+finlaysr@users.noreply.github.com>
2025-11-07 14:17:17 +0000

    ACE3 Part 2 done

--- coursework/list.c
+++ coursework/list.c
@@ -1,18 +1,19 @@
 #include "list.h"
 #include <stdlib.h>
+#include <string.h>
 
-Node *new_node(char value[100]) {
+Node *new_node(char *value) {
   // Create space for node with malloc
   Node *new = malloc(sizeof(Node));
 
   // Copy value into node and set next to null
   new->value = value;
-  int i = 0;
-  while (value[i] != 0) {
-    new->value[i] = value[i];
-    i++;
-  }
   new->next = NULL;
 
   return new;
+}
+
+void delete_node(Node *node) {
+  free(node);
+  node = NULL;
 }
