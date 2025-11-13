dd2b7146fdf5cd9baebc2f475d10f7cec86c2482
finlaysr <60317020+finlaysr@users.noreply.github.com>
2025-11-13 16:10:56 +0000

    ACE6 done, added pop function

--- coursework/list.c
+++ coursework/list.c
@@ -6,6 +6,11 @@ Node *new_node(char *value) {
   // Create space for node with malloc
   Node *new = malloc(sizeof(Node));
 
+  // Malloc failed
+  if (!new) {
+    return NULL;
+  }
+
   // Copy value into node and set next to null
   new->value = value;
   new->next = NULL;
@@ -34,6 +39,36 @@ void push(List node, char *value) {
 // List strucure
 List new_list() {
   List list = malloc(sizeof(Node **));
+
+  // Malloc failed
+  if (!list) {
+    return NULL;
+  }
   *list = NULL;
   return list;
+}
+
+char *pop(List node) {
+  // If list is empty
+  if ((*node) == NULL) {
+    return NULL;
+  }
+
+  // Singelton
+  if ((*node)->next == NULL) {
+    char *value = (*node)->value;
+    delete_node(*node);
+    *node = NULL;
+    return value;
+  }
+
+  // More than one element
+  if ((*node)->next->next == NULL) {
+    char *value = (*node)->next->value;
+    delete_node((*node)->next);
+    (*node)->next = NULL;
+    return value;
+  }
+
+  return pop(&(*node)->next);
 }
